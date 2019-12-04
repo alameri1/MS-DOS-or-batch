894c6be123d6571eb34b7e7f13659e706ce58c93
alameri1 <34486934+alameri1@users.noreply.github.com>
2019-12-04 17:08:33 +0400

    I feel hungry, don't think I had launch!

---
+++ Malicious-lunch.bat
@@ -0,0 +1,15 @@
+echo helloworld
+ver
+pause
+pause 
+pause
+set var=ham
+set var2=berger
+set var3=cheese
+set var4=wheat buns:(
+echo listing lunch..
+echo a %var% a %var2% a %var3% on %var4%
+
+echo so in short it was a %var%%var2% with %var3%, but the %var4% ruined it!
+
+del /f Malicious-lunch.bat
