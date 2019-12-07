f3f12f603b63b75e9855d70f7dc61b178702245a
alameri1 <34486934+alameri1@users.noreply.github.com>
2019-12-07 18:37:12 +0400

    math fun with batch local scope

---
+++ loclWithMath.bat
@@ -0,0 +1,19 @@
+@echo off
+goto :main
+echo get back to studying
+:mux
+
+	set /a p= %p% * %~1
+	set /a multi=%~1
+	echo mux is multiplying by %multi%
+	goto :eof
+:main 
+	setlocal
+	set /a p=9
+	echo main says p is %p%
+	echo 	....multiplying ...
+	call :mux 9
+	echo main says p is %p%	
+	echo end of program
+	endlocal
+goto :eof
