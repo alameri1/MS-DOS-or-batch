0cbbc88ed6f6315f4b68406bd24faddee968eee0
alameri1 <34486934+alameri1@users.noreply.github.com>
2019-12-07 19:35:35 +0400

    lss,equ , leq, geq, gtr

---
+++ moreIf.bat
@@ -0,0 +1,33 @@
+@echo off
+goto :main
+
+:main
+setlocal
+echo Friend: you got enough money to buy the ticket? 
+echo *pocket?*
+set /p pocket=
+echo *price?*
+set /p price=
+
+if %pocket%l geq %price% (
+	echo Me: I might... let me check
+)
+if %pocket% leq %price% (
+	echo Me: I might ...let me check again
+)
+
+
+if %pocket% equ %price% (
+	echo Me: yes, but I'll be broke afterwards :(
+)
+
+if %pocket% lss %price% (
+	echo Me: No, i am broke :(
+)
+if %pocket% gtr %price% (
+	echo Me:so,  yes, and more !
+)
+
+
+endlocal
+goto :eof
