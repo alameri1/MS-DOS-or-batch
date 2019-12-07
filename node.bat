d4b4acf454133f68628919843a6656905abeabd0
alameri1 <34486934+alameri1@users.noreply.github.com>
2019-12-07 19:49:59 +0400

    running man & logo

---
+++ node.bat
@@ -0,0 +1,30 @@
+
+@echo off
+
+
+echo
+echo                                                                                                                                                                                                                       
+echo                                                      dddddddd                                                                                                                                                         
+echo NNNNNNNN        NNNNNNNN                             d::::::d                            SSSSSSSSSSSSSSS                                        iiii                               tttt                               
+echo N:::::::N       N::::::N                             d::::::d                          SS:::::::::::::::S                                      i::::i                           ttt:::t                               
+echo N::::::::N      N::::::N                             d::::::d                         S:::::SSSSSS::::::S                                       iiii                            t:::::t                               
+echo N:::::::::N     N::::::N                             d:::::d                          S:::::S     SSSSSSS                                                                       t:::::t                               
+echo N::::::::::N    N::::::N   ooooooooooo       ddddddddd:::::d     eeeeeeeeeeee         S:::::S               ooooooooooo       cccccccccccccccciiiiiii     eeeeeeeeeeee    ttttttt:::::tttttttyyyyyyy           yyyyyyy
+echo N:::::::::::N   N::::::N oo:::::::::::oo   dd::::::::::::::d   ee::::::::::::ee       S:::::S             oo:::::::::::oo   cc:::::::::::::::ci:::::i   ee::::::::::::ee  t:::::::::::::::::t y:::::y         y:::::y 
+echo N:::::::N::::N  N::::::No:::::::::::::::o d::::::::::::::::d  e::::::eeeee:::::ee      S::::SSSS         o:::::::::::::::o c:::::::::::::::::c i::::i  e::::::eeeee:::::eet:::::::::::::::::t  y:::::y       y:::::y  
+echo N::::::N N::::N N::::::No:::::ooooo:::::od:::::::ddddd:::::d e::::::e     e:::::e       SS::::::SSSSS    o:::::ooooo:::::oc:::::::cccccc:::::c i::::i e::::::e     e:::::etttttt:::::::tttttt   y:::::y     y:::::y   
+echo N::::::N  N::::N:::::::No::::o     o::::od::::::d    d:::::d e:::::::eeeee::::::e         SSS::::::::SS  o::::o     o::::oc::::::c     ccccccc i::::i e:::::::eeeee::::::e      t:::::t          y:::::y   y:::::y    
+echo N::::::N   N:::::::::::No::::o     o::::od:::::d     d:::::d e:::::::::::::::::e             SSSSSS::::S o::::o     o::::oc:::::c              i::::i e:::::::::::::::::e       t:::::t           y:::::y y:::::y     
+echo N::::::N    N::::::::::No::::o     o::::od:::::d     d:::::d e::::::eeeeeeeeeee                   S:::::So::::o     o::::oc:::::c              i::::i e::::::eeeeeeeeeee        t:::::t            y:::::y:::::y      
+echo N::::::N     N:::::::::No::::o     o::::od:::::d     d:::::d e:::::::e                            S:::::So::::o     o::::oc::::::c     ccccccc i::::i e:::::::e                 t:::::t    tttttt   y:::::::::y       
+echo N::::::N      N::::::::No:::::ooooo:::::od::::::ddddd::::::dde::::::::e               SSSSSSS     S:::::So:::::ooooo:::::oc:::::::cccccc:::::ci::::::ie::::::::e                t::::::tttt:::::t    y:::::::y        
+echo N::::::N       N:::::::No:::::::::::::::o d:::::::::::::::::d e::::::::eeeeeeee       S::::::SSSSSS:::::So:::::::::::::::o c:::::::::::::::::ci::::::i e::::::::eeeeeeee        tt::::::::::::::t     y:::::y         
+echo N::::::N        N::::::N oo:::::::::::oo   d:::::::::ddd::::d  ee:::::::::::::e       S:::::::::::::::SS  oo:::::::::::oo   cc:::::::::::::::ci::::::i  ee:::::::::::::e          tt:::::::::::tt    y:::::y          
+echo NNNNNNNN         NNNNNNN   ooooooooooo      ddddddddd   ddddd    eeeeeeeeeeeeee        SSSSSSSSSSSSSSS      ooooooooooo       cccccccccccccccciiiiiiii    eeeeeeeeeeeeee            ttttttttttt     y:::::y           
+echo                                                                                                                                                                                                    y:::::y            
+echo                                                                                                                                                                                                   y:::::y             
+echo                                                                                                                                                                                                  y:::::y              
+echo                                                                                                                                                                                                 y:::::y               
+echo                                                                                                                                                                                                yyyyyyy                
+echo                                                                                                                                                                                                                       
+                                                                                                                                                                                                                                                                                                                           
